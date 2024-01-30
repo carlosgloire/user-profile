7e3f1113f6444d4228976057d2aa921518dfe77e
carlosgloire <ndayisabagloire96@gmail.com>
2024-01-31 01:27:02 +1100

    add this to my rep

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,7 @@
-document.getElementById("openPopup1").addEventListener("click", function() {
-    document.getElementById("popup1").style.display = "block";
+document.getElementById("openPopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
 });
 
-document.getElementById("closePopup1").addEventListener("click", function(){
-    document.getElementById("popup1").style.display = "none";
+document.getElementById("closePopup").addEventListener("click", function(){
+    document.getElementById("popup").style.display = "none";
 }); 
