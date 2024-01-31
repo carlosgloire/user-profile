a5e3702f07fa519a41ac4db3c78967cce905e4cf
carlosgloire <ndayisabagloire96@gmail.com>
2024-01-31 13:52:32 +1100

    add this

--- popup/popup.js
+++ popup/popup.js
@@ -5,3 +5,11 @@ document.getElementById("openPopup").addEventListener("click", function() {
 document.getElementById("closePopup").addEventListener("click", function(){
     document.getElementById("popup").style.display = "none";
 }); 
+
+document.getElementById("openPopup1").addEventListener("click", function() {
+    document.getElementById("popup1").style.display = "block";
+});
+
+document.getElementById("closePopup1").addEventListener("click", function(){
+    document.getElementById("popup1").style.display = "none";
+}); 
